0cbefa68c676d5cfb823406a451f3fa4a153b599
Paulo Matozinho <paulo.matozinho@gmail.com>
2023-08-04 19:40:57 -0300

    finalizado JavaScript do tema dark/light mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,55 @@
 // Grab elements
-const selectElemento = selector => {
+const selectElement = selector => {
     const element = document.querySelector(selector)
     if (element) return element;
     throw new Error(`Algo de errado, verifique se ${selector} existe ou se foi digitado corretamente.`);
 };
 
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if (this.scrollY >= 15) {
+        headerElement.classlist.add('activated');
+    } else {
+        headerElement.classlist.remove('activated');
+    }
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('light-theme')
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+
+    if (bodyElement.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
+
 
 // Swiper
